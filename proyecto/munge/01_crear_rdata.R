075ebf8781c100b6b9a216d9ea5badc1a8da136b
Songeo <sonia.gmcl@gmail.com>
2017-05-06 14:22:20 -0500

    datos con forma binaria 0 y 1

--- proyecto/munge/01_crear_rdata.R
+++ proyecto/munge/01_crear_rdata.R
@@ -18,9 +18,36 @@ df.cpj.17 <- readxl::read_excel("data/cpj-database_mod.xls") %>%
          year = ifelse(year < 92, year + 2000, year))
 df.cpj.17
 
-df.cpj.17 %>% head
+df.cpj.17 %>% data.frame() %>% head
 df.cpj.17 %>% dim
 str(df.cpj.17)
+
+
+
+# Binarias de categoricas multiples
+aux <- df.cpj.17 %>% 
+  dplyr::select(Medium, Job, Coverage) %>% 
+  rownames_to_column() %>% 
+  gather(var.lab, var.val, -rowname) %>% 
+  separate( var.val , c("1", "2", "3", "4", "5", "6", "7", "8"), sep = ",") %>% 
+  gather(col.num, col.val, c(-rowname, -var.lab)) %>% 
+  na.omit() %>% 
+  mutate(ind = 1,
+         col.val = str_trim(col.val)) %>% 
+  dplyr::select(-col.num) %>% 
+  unite_("var_col", c('var.lab', 'col.val'), sep = "_") %>% 
+  spread(var_col, ind, fill = 0)
+aux
+dim(aux)
+apply(is.na(aux), 2, sum)
+
+aux %>% data.frame() %>% head
+
+table(df.cpj.17$motive)
+df.cpj.17 <- df.cpj.17 %>% 
+  rownames_to_column() %>% 
+  left_join(aux) 
+df.cpj.17 %>% dim
 
 
 cache("df.cpj.17")
